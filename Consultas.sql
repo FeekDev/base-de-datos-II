4d009174477e7c23739d4e5e243796e67350aac4
FeekDev <felipe.riosgg@gmail.com>
2026-03-07 21:25:55 -0500

    Add new SQL queries for data analysis and insertions, including product sales and customer data

--- Consultas.sql
+++ Consultas.sql
@@ -6,7 +6,6 @@ use productos
 go
 
 -- 1) INNER JOIN (Ventas con Cliente y Vendedor)
-
 SELECT v.codigo_vta, c.nombre, ve.nombre
 FROM venta v
 INNER JOIN cliente c ON
@@ -15,7 +14,6 @@ INNER JOIN vendedor ve ON
 v.doc_vendor = ve.documento
 
 -- 2)  INNER JOIN con detalle (Ventas + Productos)
-
 SELECT i.codigo_vta, p.nombre, 
 i.cantidad, p.valor, SUM(i.cantidad * i.valor_vta) AS Subtotal
 FROM items_venta i
@@ -24,7 +22,6 @@ i.codigo_pdt = p.codigo
 GROUP BY i.codigo_vta, p.nombre, i.cantidad, p.valor;
 
 -- 3) LEFT JOIN (Clientes sin compras)
-
 SELECT c.documento, c.nombre, v.codigo_vta, v.fecha
 FROM cliente c -- Izquierda (preferente)
 LEFT JOIN venta v ON -- Derecha
@@ -32,7 +29,6 @@ c.documento = v.doc_cli
 ORDER BY c.nombre
 
 -- 4) RIGHT JOIN (Productos no vendidos)
-
 SELECT p.codigo, p.nombre, i.cantidad
 FROM items_venta i -- Izquierda
 RIGHT JOIN productos p ON -- Derecha  (Preferente)
@@ -47,8 +43,57 @@ GROUP BY c.nombre
 
 -- 6) Total vendido por vendedor
 SELECT ve.nombre, SUM(v.valor) AS Total_vendido, 
-COUNT(v.factura_num) AS Número_de_ventas
+COUNT(v.factura_num) AS Nï¿½mero_de_ventas
 FROM vendedor ve
 JOIN venta v ON
 ve.documento = v.doc_vendor
 GROUP BY ve.nombre
+
+-- 7) Producto mï¿½s vendido (por cantidad)
+SELECT p.nombre, COUNT(i.cantidad) AS Cantidad
+FROM productos p
+JOIN items_venta i ON
+p.codigo = i.codigo_pdt
+GROUP BY p.nombre
+
+
+-- 8) Precio mï¿½ximo y mï¿½nimo vendido por producto
+SELECT p.nombre, MAX(i.valor_vta * i.cantidad) AS Mï¿½ximo, 
+MIN(i.valor_vta) AS Mï¿½nimo
+FROM productos p
+JOIN  items_venta i ON
+p.codigo = i.codigo_pdt
+GROUP BY p.nombre
+
+-- 9 ï¿½ Clientes que hayan comprado mï¿½s de 1.000.000
+SELECT c.nombre, SUM(v.valor) AS Total_Comprado
+FROM cliente c
+JOIN venta v  ON
+c.documento = v.doc_cli
+GROUP BY c.nombre
+HAVING SUM(v.valor) > 1000000
+
+-- 10 ï¿½ Productos con mï¿½s de 50 unidades vendidas
+SELECT p.nombre, SUM(i.cantidad) AS Unidades_vendidas
+FROM productos p
+JOIN items_venta i  ON
+p.codigo = i.codigo_pdt
+GROUP BY p.nombre
+HAVING SUM(i.cantidad) > 50
+
+-- 11 ï¿½ Reporte completo de ventas por fecha
+SELECT SUM(v.valor * i.cantidad) AS Valor_vendido , 
+COUNT(v.factura_num) AS Num_Facturas, AVG(v.valor) AS Valor_promedio_X_Venta
+FROM venta v
+JOIN items_venta i  ON
+v.codigo_vta = i.codigo_vta
+GROUP BY v.fecha
+
+-- 12 Mejor vendedor
+SELECT TOP 1 ven.nombre, SUM(v.valor) AS Total_vendido
+FROM venta v
+JOIN vendedor ven ON
+ven.documento = v.doc_vendor
+GROUP BY ven.nombre
+ORDER BY Total_vendido DESC
+
--- Entrega.sql
+++ Entrega.sql
@@ -1,3 +1,58 @@
+-- =====================================================
+-- CREACIÓN DE BASE DE DATOS Y TABLAS
+-- =====================================================
+
+IF OBJECT_ID('productos') is not null
+ drop database productos
+create database productos;
+
+GO 
+USE productos
+GO 
+
+CREATE TABLE vendedor(
+	documento numeric(12,0) NOT NULL PRIMARY KEY,
+	nombre varchar(35),
+	tel numeric(18,0),
+	dir varchar(50),
+);
+
+CREATE TABLE cliente (
+	documento numeric(12,0) NOT NULL PRIMARY KEY,
+	nombre varchar(35),
+	dir varchar(50),
+	cod_pais int,
+	tel numeric(15,0)
+);
+
+CREATE TABLE venta(
+	codigo_vta int NOT NULL IDENTITY(1,1) PRIMARY KEY ,
+	valor numeric(18,0),
+	fecha date,
+	factura_num int,
+	doc_vendor numeric(12,0) FOREIGN KEY REFERENCES vendedor(documento),
+	doc_cli numeric(12,0) FOREIGN KEY REFERENCES cliente(documento),
+);
+
+CREATE TABLE productos(
+	codigo int NOT NULL PRIMARY KEY IDENTITY(1,1),
+	valor numeric(18,0),
+	cantidad int,
+	nombre varchar(50)
+);
+
+CREATE TABLE items_venta(
+	codigo_pdt int FOREIGN KEY REFERENCES productos(codigo),
+	codigo_vta int FOREIGN KEY REFERENCES venta(codigo_vta),
+	id int NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	valor_vta numeric(18,0),
+	cantidad int
+);
+
+-- =====================================================
+-- INSERCIONES Y ALTERACIONES
+-- =====================================================
+
 use productos
 go
 
@@ -77,3 +132,106 @@ INSERT INTO items_venta (codigo_pdt, codigo_vta, valor_vta, cantidad) VALUES (10
 INSERT INTO items_venta (codigo_pdt, codigo_vta, valor_vta, cantidad) VALUES (10, 21, 55000000, 3);
 INSERT INTO items_venta (codigo_pdt, codigo_vta, valor_vta, cantidad) VALUES (12, 22, 600000, 1);
 INSERT INTO items_venta (codigo_pdt, codigo_vta, valor_vta, cantidad) VALUES (12, 23, 30000000, 50);
+
+-- =====================================================
+-- CONSULTAS
+-- =====================================================
+
+-- Ejercicios Consulta  --
+
+-- Definir base de datos a usar --
+
+use productos
+go
+
+-- 1) INNER JOIN (Ventas con Cliente y Vendedor)
+SELECT v.codigo_vta, c.nombre, ve.nombre
+FROM venta v
+INNER JOIN cliente c ON
+v.doc_cli = c.documento
+INNER JOIN vendedor ve ON
+v.doc_vendor = ve.documento
+
+-- 2)  INNER JOIN con detalle (Ventas + Productos)
+SELECT i.codigo_vta, p.nombre, 
+i.cantidad, p.valor, SUM(i.cantidad * i.valor_vta) AS Subtotal
+FROM items_venta i
+INNER JOIN productos p ON
+i.codigo_pdt = p.codigo
+GROUP BY i.codigo_vta, p.nombre, i.cantidad, p.valor;
+
+-- 3) LEFT JOIN (Clientes sin compras)
+SELECT c.documento, c.nombre, v.codigo_vta, v.fecha
+FROM cliente c -- Izquierda (preferente)
+LEFT JOIN venta v ON -- Derecha
+c.documento = v.doc_cli
+ORDER BY c.nombre
+
+-- 4) RIGHT JOIN (Productos no vendidos)
+SELECT p.codigo, p.nombre, i.cantidad
+FROM items_venta i -- Izquierda
+RIGHT JOIN productos p ON -- Derecha  (Preferente)
+p.codigo = i.codigo_pdt
+
+-- 5)  Total vendido por cliente
+SELECT c.nombre, SUM(v.valor) AS Valor, COUNT(v.factura_num) AS Facturas
+FROM cliente c
+JOIN venta v ON
+c.documento = v.doc_cli
+GROUP BY c.nombre
+
+-- 6) Total vendido por vendedor
+SELECT ve.nombre, SUM(v.valor) AS Total_vendido, 
+COUNT(v.factura_num) AS Número_de_ventas
+FROM vendedor ve
+JOIN venta v ON
+ve.documento = v.doc_vendor
+GROUP BY ve.nombre
+
+-- 7) Producto más vendido (por cantidad)
+SELECT p.nombre, COUNT(i.cantidad) AS Cantidad
+FROM productos p
+JOIN items_venta i ON
+p.codigo = i.codigo_pdt
+GROUP BY p.nombre
+
+
+-- 8) Precio máximo y mínimo vendido por producto
+SELECT p.nombre, MAX(i.valor_vta * i.cantidad) AS Máximo, 
+MIN(i.valor_vta) AS Mínimo
+FROM productos p
+JOIN  items_venta i ON
+p.codigo = i.codigo_pdt
+GROUP BY p.nombre
+
+-- 9 – Clientes que hayan comprado más de 1.000.000
+SELECT c.nombre, SUM(v.valor) AS Total_Comprado
+FROM cliente c
+JOIN venta v  ON
+c.documento = v.doc_cli
+GROUP BY c.nombre
+HAVING SUM(v.valor) > 1000000
+
+-- 10 – Productos con más de 50 unidades vendidas
+SELECT p.nombre, SUM(i.cantidad) AS Unidades_vendidas
+FROM productos p
+JOIN items_venta i  ON
+p.codigo = i.codigo_pdt
+GROUP BY p.nombre
+HAVING SUM(i.cantidad) > 50
+
+-- 11 – Reporte completo de ventas por fecha
+SELECT SUM(v.valor * i.cantidad) AS Valor_vendido , 
+COUNT(v.factura_num) AS Num_Facturas, AVG(v.valor) AS Valor_promedio_X_Venta
+FROM venta v
+JOIN items_venta i  ON
+v.codigo_vta = i.codigo_vta
+GROUP BY v.fecha
+
+-- 12 Mejor vendedor
+SELECT TOP 1 ven.nombre, SUM(v.valor) AS Total_vendido
+FROM venta v
+JOIN vendedor ven ON
+ven.documento = v.doc_vendor
+GROUP BY ven.nombre
+ORDER BY Total_vendido DESC
