18627a45d42439edd27dfebd2306a6b1794bb3fb
FeekDev <felipe.riosgg@gmail.com>
2026-04-01 12:18:47 -0500

    procedimiento crear_vta

--- Taller Integrador – Sistema de Ventas en SQL Server.sql
+++ Taller Integrador – Sistema de Ventas en SQL Server.sql
@@ -1,7 +1,7 @@
 -- Funcion para calcular subtotal ---
 CREATE FUNCTION calcular_subtotal
 (
-    @precio numeric(18,0)
+    @precio numeric(18,0),
     @cantidad int
 )
 RETURNS numeric(18,0)
@@ -14,3 +14,23 @@ END
 SELECT codigo_vta, cantidad, valor_vta, 
 dbo.calcular_subtotal(valor_vta, cantidad) as subtotal
 FROM items_venta
+
+-- Procedimiento para insertar una venta ---
+ALTER PROCEDURE sp_CrearVenta
+    @valor numeric(18, 0) = 0,
+    @fecha DATE = NULL,
+    @doc_vendor numeric(12,0),
+    @doc_cli numeric(12,0)
+AS
+BEGIN
+    IF @fecha IS NULL
+        SET @fecha = CONVERT(DATE, GETDATE())
+    Insert into venta(valor, fecha, doc_vendor, doc_cli )
+    values (@valor, @fecha, @doc_vendor, @doc_cli)
+END
+
+EXECsp_CrearVenta
+    @doc_vendor = 555666,
+    @doc_cli = 2011
+    SELECT top 1 codigo_vta from venta
+    ORDER BY codigo_vta DESC 
