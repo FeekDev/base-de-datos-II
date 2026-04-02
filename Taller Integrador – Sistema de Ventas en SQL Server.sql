43826642c49eebcb8fbb42bf9aac62bb579be0de
FeekDev <felipe.riosgg@gmail.com>
2026-04-02 13:41:01 -0500

    completar procedimiento venta

--- Taller Integrador – Sistema de Ventas en SQL Server.sql
+++ Taller Integrador – Sistema de Ventas en SQL Server.sql
@@ -19,18 +19,29 @@ FROM items_venta
 ALTER PROCEDURE sp_CrearVenta
     @valor numeric(18, 0) = 0,
     @fecha DATE = NULL,
+    @factura_num INT = NULL,
     @doc_vendor numeric(12,0),
     @doc_cli numeric(12,0)
 AS
 BEGIN
-    IF @fecha IS NULL
+    IF @fecha IS NULL AND @factura_num IS NULL-- se debe agregar para poder convertir la fecha a formato date --
         SET @fecha = CONVERT(DATE, GETDATE())
-    Insert into venta(valor, fecha, doc_vendor, doc_cli )
-    values (@valor, @fecha, @doc_vendor, @doc_cli)
+        SET @factura_num = @factura_num + 1
+    Insert into venta(valor, fecha, factura_num, doc_vendor, doc_cli )
+    values (@valor, @fecha,@factura_num, @doc_vendor, @doc_cli)
 END
 
-EXECsp_CrearVenta
+EXEC sp_CrearVenta
     @doc_vendor = 555666,
     @doc_cli = 2011
-    SELECT top 1 codigo_vta from venta
-    ORDER BY codigo_vta DESC 
+SELECT top 1 codigo_vta from venta
+    ORDER BY codigo_vta DESC
+
+ -- agregar producto a la venta ---
+ CREATE PROCEDURE sp_AgregarProductoVenta
+ @id_venta
+ @id_producto
+ @cantidad
+ As
+ BEGIN
+ INSERT INTO items_venta()
