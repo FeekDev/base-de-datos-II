f038a4797d93c16949edf85119fb5b142d154d89
FeekDev <felipe.riosgg@gmail.com>
2026-04-20 18:20:03 -0500

    trigger

--- trigger.sql
+++ trigger.sql
@@ -1,18 +1,17 @@
 CREATE TRIGGER trv_vlidacion
-ON itemsVenta
+ON itemsVenta -- Nombre de la tabla a la que se le aplicará el trigger
 INSTEAD OF INSERT
 AS
 BEGIN
-IF EXIST(SELECT * FROM inserted i
-join productos p on i.idProducto = p.idProducto
-where i.cantidadItems > p.cantidadDisponible)
+IF EXIST(SELECT * FROM inserted i -- La tabla "inserted" es una tabla virtual que contiene los datos que se están intentando insertar
+join productos p on i.idProducto = p.idProducto -- Se une la tabla "inserted" con la tabla "productos" para obtener la cantidad disponible del producto
+where i.cantidadItems > p.cantidadDisponible) --  Se verifica si la cantidad de items que se intenta insertar es mayor a la cantidad disponible del producto
 BEGIN
     print"No es posible agregar el item de venta"
 END
 ELSE
-    insert into itemsVenta(idVenta, idProducto, cantidadItems)
-    SELECT idVenta, idProducto, cantidadItems FROM inserted
-END
+    insert into itemsVenta(idVenta, idProducto, cantidadItems) -- se pone por ser identity el idtem 
+    select idVenta, idProducto, cantidadItems from inserted 
 END
 
 insert into itemsVenta(idVenta, idProducto, cantidadItems)
